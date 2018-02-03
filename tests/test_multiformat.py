81ccb77e34f659a8b2de524b5114d6aecccf5309
Adam Moller <31048865+AdamMoller@users.noreply.github.com>
2018-02-03 11:43:47 -0500

    replace init tests with class

--- tests/test_multiformat.py
+++ tests/test_multiformat.py
@@ -1,28 +1,26 @@
 import pytest
-
 from multiformat.multiformat import Document
 
 
-def test_dimensions():
-    with pytest.raises(KeyError):
-        document = Document("a44", "landscape")
+class TestInit():
+    @pytest.mark.parametrize("doc_size,doc_layout,match", [
+        ("a44", "landscape", "Invalid document size: 'a44'"),
+        ("A4", "L", "Invalid document layout: 'L'"),
+    ])
+    def test_invalid_parameters(self, doc_size, doc_layout, match):
+        with pytest.raises(KeyError, match=match):
+            document = Document(doc_size, doc_layout)
 
-
-def test_dimensions():
-    with pytest.raises(KeyError):
-        document = Document("letter", "L")
-
-
-@pytest.mark.parametrize("doc_size,doc_layout,w,h", [
-    ("a4", "Portrait", 2100, 2970),
-    ("A4", "landscape", 2970, 2100),
-    ("letter", "portrait", 2159, 2794),
-    ("Letter", "Landscape", 2794, 2159),
-])
-def test_dimensions(doc_size, doc_layout, w, h):
-    document = Document(doc_size, doc_layout)
-    assert document.w == w
-    assert document.h == h
+    @pytest.mark.parametrize("doc_size,doc_layout,w,h", [
+        ("a4", "Portrait", 2100, 2970),
+        ("A4", "landscape", 2970, 2100),
+        ("letter", "portrait", 2159, 2794),
+        ("Letter", "Landscape", 2794, 2159),
+    ])
+    def test_dimensions(self, doc_size, doc_layout, w, h):
+        document = Document(doc_size, doc_layout)
+        assert document.w == w
+        assert document.h == h
 
 
 def test_draw_string_success():
