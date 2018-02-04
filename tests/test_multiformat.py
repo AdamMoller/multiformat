d30c99719e9a8b1fc7173b510d370729679f5ffc
Adam Moller <31048865+AdamMoller@users.noreply.github.com>
2018-02-04 12:10:23 -0500

    add test for page break

--- tests/test_multiformat.py
+++ tests/test_multiformat.py
@@ -194,6 +194,19 @@ class TestDrawing:
             document.draw_line(x, y, x1, y1, width, color)
 
 
+class TestMarkers:
+    def new_document(self):
+        return Document("letter", "portrait")
+
+    def test_insert_page_break(self):
+        document = self.new_document()
+        document.draw_rectangle(0, 0, 200, 200, (0, 0, 0), (0, 0, 0), 1)
+        document.insert_page_break()
+        assert document._document[-1] == {
+            "type": "page_break",
+        }
+
+
 def test_generate_unsupported_image():
     document = Document("letter", "portrait")
     with pytest.raises(RuntimeError):
